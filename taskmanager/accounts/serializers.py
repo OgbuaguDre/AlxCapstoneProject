a8af86cd1afb0b88c675ee3336643f11ebb61e7c
Chima Christian <123310155+OgbuaguDre@users.noreply.github.com>
2025-08-16 14:44:27 +0100

    Create serializers.py

---
+++ taskmanager/accounts/serializers.py
@@ -0,0 +1,27 @@
+from django.contrib.auth.models import User
+from rest_framework import serializers
+from django.contrib.auth.password_validation import validate_password
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ["id", "username", "email", "date_joined", "is_active"]
+        read_only_fields = ["id", "date_joined", "is_active"]
+
+class RegisterSerializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True)
+
+    class Meta:
+        model = User
+        fields = ["username", "email", "password"]
+
+    def validate_password(self, value):
+        validate_password(value)
+        return value
+
+    def create(self, validated_data):
+        return User.objects.create_user(
+            username=validated_data["username"],
+            email=validated_data.get("email"),
+            password=validated_data["password"],
+        )
