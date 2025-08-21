b83c6e77a16506d9a1dbbf6217b91ec42f1457d7
Chima Christian <123310155+OgbuaguDre@users.noreply.github.com>
2025-08-21 19:16:08 +0100

    Update views.py

--- taskmanager/accounts/views.py
+++ taskmanager/accounts/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from rest_framework import generics, permissions
 from django.contrib.auth.models import User
 from .serializers import RegisterSerializer, UserSerializer
+from rest_framework import viewsets, permissions
+
 
 class RegisterView(generics.CreateAPIView):
     queryset = User.objects.all()
@@ -13,3 +15,11 @@ class MeView(generics.RetrieveUpdateAPIView):
 
     def get_object(self):
         return self.request.user
+
+class UserViewSet(viewsets.ModelViewSet):
+    """
+    Admin-only CRUD for users.
+    """
+    queryset = User.objects.all().order_by("id")
+    serializer_class = UserSerializer
+    permission_classes = [permissions.IsAdminUser]
