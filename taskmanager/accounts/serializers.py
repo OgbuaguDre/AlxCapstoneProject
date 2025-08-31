eebb88a7f9aea163bf420340092074e302817c0e
Chima Christian <123310155+OgbuaguDre@users.noreply.github.com>
2025-08-31 21:42:48 +0100

    Update serializers.py

--- taskmanager/accounts/serializers.py
+++ taskmanager/accounts/serializers.py
@@ -5,23 +5,24 @@ from django.contrib.auth.password_validation import validate_password
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ["id", "username", "email", "date_joined", "is_active"]
+        fields = ["id", "username", "email", "date_joined", "is_active", "password"]
         read_only_fields = ["id", "date_joined", "is_active"]
+        write_only_fields = ["password"]
 
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+# class RegisterSerializer(serializers.ModelSerializer):
+#     password = serializers.CharField(write_only=True)
 
-    class Meta:
-        model = User
-        fields = ["username", "email", "password"]
+#     class Meta:
+#         model = User
+#         fields = ["username", "email", "password"]
 
-    def validate_password(self, value):
-        validate_password(value)
-        return value
+#     def validate_password(self, value):
+#         validate_password(value)
+#         return value
 
-    def create(self, validated_data):
-        return User.objects.create_user(
-            username=validated_data["username"],
-            email=validated_data.get("email"),
-            password=validated_data["password"],
-        )
+#     def create(self, validated_data):
+#         return User.objects.create_user(
+#             username=validated_data["username"],
+#             email=validated_data.get("email"),
+#             password=validated_data["password"],
+#         )
