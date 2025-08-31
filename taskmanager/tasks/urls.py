4b549e6b34857acbc0ae98daa6de9355a406019d
Chima Christian <123310155+OgbuaguDre@users.noreply.github.com>
2025-08-31 21:56:11 +0100

    Update urls.py

--- taskmanager/tasks/urls.py
+++ taskmanager/tasks/urls.py
@@ -1,13 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import RegisterView, LoginView, LogoutView, TaskViewSet
+from .views import TaskViewSet
 
 router = DefaultRouter()
 router.register(r'tasks', TaskViewSet, basename='task')
 
 urlpatterns = [
-    path("register/", RegisterView.as_view(), name="register"),
-    path("login/", LoginView.as_view(), name="login"),
-    path("logout/", LogoutView.as_view(), name="logout"),
+    # path("register/", RegisterView.as_view(), name="register"),
+    # path("login/", LoginView.as_view(), name="login"),
+    # path("logout/", LogoutView.as_view(), name="logout"),
     path("", include(router.urls)),  # tasks endpoints
 ]
