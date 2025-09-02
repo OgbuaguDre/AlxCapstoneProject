dfa390c4df6e76a39057d2a9daead775ae8045f1
Chima Christian <123310155+OgbuaguDre@users.noreply.github.com>
2025-09-02 20:18:43 +0100

    Update serializers.py

--- taskmanager/tasks/serializers.py
+++ taskmanager/tasks/serializers.py
@@ -27,30 +27,3 @@ class TaskSerializer(serializers.ModelSerializer):
             )
         return super().update(instance, validated_data)
 
-class RegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True, required=True, validators=[validate_password])
-    password2 = serializers.CharField(write_only=True, required=True)  # confirm password
-
-    class Meta:
-        model = User
-        fields = ['username', 'email', 'password', 'password2']
-
-    def validate(self, attrs):
-        if attrs['password'] != attrs['password2']:
-            raise serializers.ValidationError({"password": "Passwords must match."})
-        return attrs
-
-    def create(self, validated_data):
-        user = User.objects.create(
-            username=validated_data['username'],
-            email=validated_data['email']
-        )
-        user.set_password(validated_data['password'])
-        user.save()
-        return user
-
-
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = ['id', 'username', 'email']
